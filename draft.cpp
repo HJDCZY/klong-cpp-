05da029dd6a0fdee61997d4e78dfbd4c0aaf4e46
Ruibo Cai <1751114377@qq.com>
2023-05-14 22:42:33 +0800

    更新了10.4，draft是数据结构草稿，忽略就好

---
+++ draft.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverse(string &str){
+    int len = str.length();
+    string result[len];
+    result
+}
+
+int main(){
+    
+
+return 0;
+}
+
